e5579fe0712e538e22d37a280998225135e3567c
Anderson Lopez <anderson.lpz.desenv@gmail.com>
2024-09-22 21:55:14 -0300

    feat: create main.tf

---
+++ main.tf
@@ -0,0 +1,68 @@
+# Configuração do Terraform
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.52.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.4.3"
+    }
+  }
+  required_version = ">= 1.1.0"
+
+  # Configuração do Terraform Cloud
+  cloud {
+    organization = "SofArc6Soat"
+
+    workspaces {
+      name = "quickfood-backend"
+    }
+  }
+}
+
+# Configuração do provedor AWS
+provider "aws" {
+  region = "us-east-1" 
+}
+
+resource "random_pet" "sg" {}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+# Recurso EC2 para rodar o backend
+resource "aws_instance" "backend" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+  
+  # Conectar à segurança da rede
+  vpc_security_group_ids = [aws_security_group.backend-sg.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              # Script de inicialização da instância EC2
+              apt-get update
+              apt-get install -y docker.io
+              systemctl start docker
+              docker run -d -p 80:80 sofarc6soat/quickfood-backend:latest
+              EOF
+
+  tags = {
+    Name = "Quickfood Backend Server"
+  }
+}
