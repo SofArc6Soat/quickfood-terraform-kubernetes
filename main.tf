886e78ec15a681288e5b20f69d3f0d77dc14d385
Anderson Lopez <anderson.lpz.desenv@gmail.com>
2024-09-29 19:59:23 -0300

    feat: adjusting files

--- main.tf
+++ main.tf
@@ -12,7 +12,6 @@ terraform {
   }
   required_version = ">= 1.1.0"
 
-  # Configuração do Terraform Cloud
   cloud {
     organization = "SofArc6Soat"
 
@@ -22,8 +21,7 @@ terraform {
   }
 }
 
-
-# Configuração do provedor AWS
+# Provedor AWS
 provider "aws" {
   region = "us-east-1" 
 }
@@ -46,8 +44,8 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"] # Canonical
 }
 
-# Recurso de grupo de segurança
-resource "aws_security_group" "backend-sg" {
+# Recurso de grupo de segurança do backend
+resource "aws_security_group" "backend_sg" {
   name        = "backend-sg"
   description = "Security group for the backend instance"
 
@@ -66,13 +64,24 @@ resource "aws_security_group" "backend-sg" {
   }
 }
 
-# Recurso EC2 para rodar o backend
+# Recurso null para verificar a criação da instância SQL Server
+resource "null_resource" "check_sql_instance" {
+  depends_on = [aws_instance.sqlserver]
+
+  provisioner "local-exec" {
+    command = "echo 'A instância do SQL Server precisa ser criada primeiro antes de criar a instância do backend.'"
+  }
+}
+
+# Instância EC2 para rodar o backend
 resource "aws_instance" "backend" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
   
-  # Conectar à segurança da rede
-  vpc_security_group_ids = [aws_security_group.backend-sg.id]
+  # Conectar à segurança da rede usando as configurações exportadas do SQL
+  vpc_security_group_ids = [aws_security_group.backend_sg.id]
+
+  subnet_id = "subnet_id" # Use a saída da VPC para referenciar a subnet
 
   user_data = <<-EOF
               #!/bin/bash
@@ -80,10 +89,17 @@ resource "aws_instance" "backend" {
               apt-get update
               apt-get install -y docker.io
               systemctl start docker
-              docker run -d -p 80:80 sofarc6soat/quickfood-backend:latest
+              
+              # Obtendo o IP da instância SQL Server
+              SQL_SERVER_IP="${aws_instance.sqlserver.private_ip}"
+              
+              # Executando o contêiner do backend
+              docker run -d -p 80:80 -e SQL_SERVER_IP=$SQL_SERVER_IP sofarc6soat/quickfood-backend:latest
               EOF
 
   tags = {
     Name = "Quickfood Backend Server"
   }
+
+  depends_on = [null_resource.check_sql_instance] # Garante que a mensagem de verificação seja executada antes da criação do backend
 }
