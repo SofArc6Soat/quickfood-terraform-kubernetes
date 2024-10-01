1a033d9766f3b78b8e63d1a9f9b8759351790fc2
Anderson Lopez <anderson.lpz.desenv@gmail.com>
2024-09-30 22:12:58 -0300

    feat: teste

--- main.tf
+++ main.tf
@@ -12,6 +12,7 @@ terraform {
   }
   required_version = ">= 1.1.0"
 
+  # Configuração do Terraform Cloud
   cloud {
     organization = "SofArc6Soat"
 
@@ -21,46 +22,32 @@ terraform {
   }
 }
 
-# Provedor AWS
+
+# Configuração do provedor AWS
 provider "aws" {
-  region = "us-east-1"
+  region = "us-east-1" 
 }
 
-# Data source para buscar a AMI do Ubuntu
+resource "random_pet" "sg" {}
+
 data "aws_ami" "ubuntu" {
   most_recent = true
-
-  owners = ["099720109477"] # ID do proprietário da AMI do Ubuntu (Canonical)
 
   filter {
     name   = "name"
     values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
   }
-}
 
-# Data source para verificar se a instância SQL Server existe
-data "aws_instance" "sqlserver" {
   filter {
-    name   = "tag:Name"
-    values = ["Quickfood SQL Server"]  # Certifique-se de que a tag está correta
+    name   = "virtualization-type"
+    values = ["hvm"]
   }
+
+  owners = ["099720109477"] # Canonical
 }
 
-# Recurso null para gerar erro se a instância SQL Server não for encontrada
-resource "null_resource" "check_sql_instance" {
-  count = data.aws_instance.sqlserver.id != "" ? 0 : 1
-
-  provisioner "local-exec" {
-    command = <<-EOT
-      echo "ERRO: A instância do SQL Server chamada 'Quickfood SQL Server' não foi encontrada."
-      echo "Certifique-se de que a instância foi criada, que a tag 'Name' está correta e que está na região us-east-1."
-      exit 1
-    EOT
-  }
-}
-
-# Recurso de grupo de segurança para o backend
-resource "aws_security_group" "backend_sg" {
+# Recurso de grupo de segurança
+resource "aws_security_group" "backend-sg" {
   name        = "backend-sg"
   description = "Security group for the backend instance"
 
@@ -79,34 +66,24 @@ resource "aws_security_group" "backend_sg" {
   }
 }
 
-# Instância EC2 para rodar o backend
+# Recurso EC2 para rodar o backend
 resource "aws_instance" "backend" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
   
-  vpc_security_group_ids = [aws_security_group.backend_sg.id]
+  # Conectar à segurança da rede
+  vpc_security_group_ids = [aws_security_group.backend-sg.id]
 
   user_data = <<-EOF
               #!/bin/bash
+              # Script de inicialização da instância EC2
               apt-get update
               apt-get install -y docker.io
               systemctl start docker
-              
-              # Obtendo o IP da instância SQL Server
-              SQL_SERVER_IP="${data.aws_instance.sqlserver.private_ip}"
-              
-              # Executando o contêiner do backend
-              docker run -d -p 80:80 -e SQL_SERVER_IP=$SQL_SERVER_IP sofarc6soat/quickfood-backend:latest
+              docker run -d -p 80:80 sofarc6soat/quickfood-backend:latest
               EOF
 
   tags = {
     Name = "Quickfood Backend Server"
   }
-
-  depends_on = [null_resource.check_sql_instance]  # Só cria o backend se a instância SQL Server existir
-}
-
-# Saída para o IP do SQL Server
-output "sql_server_ip" {
-  value = data.aws_instance.sqlserver.private_ip
 }
