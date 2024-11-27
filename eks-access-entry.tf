4755e9caaa121293708d81c2dc4e33e5585fa15d
Anderson Lopez <anderson.lpz.desenv@gmail.com>
2024-11-27 17:59:24 -0300

    feat: aws_eks_node_group

--- eks-access-entry.tf
+++ eks-access-entry.tf
@@ -1,5 +1,26 @@
-resource "aws_eks_access_entry" "eks-access-entry" {
-  cluster_name      = aws_eks_cluster.eks-cluster.name
+resource "aws_eks_access_entry" "eks-access-entry-backoffice" {
+  cluster_name      = aws_eks_cluster.eks-cluster-backoffice.name
+  principal_arn     = var.principalArn
+  kubernetes_groups = ["quickfood"]
+  type              = "STANDARD"
+}
+
+resource "aws_eks_access_entry" "eks-access-entry-pagamento" {
+  cluster_name      = aws_eks_cluster.eks-cluster-pagamento.name
+  principal_arn     = var.principalArn
+  kubernetes_groups = ["quickfood"]
+  type              = "STANDARD"
+}
+
+resource "aws_eks_access_entry" "eks-access-entry-pedido" {
+  cluster_name      = aws_eks_cluster.eks-cluster-pedido.name
+  principal_arn     = var.principalArn
+  kubernetes_groups = ["quickfood"]
+  type              = "STANDARD"
+}
+
+resource "aws_eks_access_entry" "eks-access-entry-producao" {
+  cluster_name      = aws_eks_cluster.eks-cluster-producao.name
   principal_arn     = var.principalArn
   kubernetes_groups = ["quickfood"]
   type              = "STANDARD"
