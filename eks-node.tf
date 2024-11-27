7f2447b2060436c680ff0938a9d760d039d2cb5b
Anderson Lopez <anderson.lpz.desenv@gmail.com>
2024-11-27 20:01:46 -0300

    feat: backend

--- eks-node.tf
+++ eks-node.tf
@@ -1,6 +1,6 @@
 resource "aws_eks_node_group" "eks-node-backoffice" {
   cluster_name    = aws_eks_cluster.eks-cluster-backoffice.name
-  node_group_name = "${var.nodeGroup}-node-group--backoffice"
+  node_group_name = "${var.nodeGroup}-node-group-backoffice"
   node_role_arn   = var.labRole
   subnet_ids      = [for subnet in data.aws_subnet.subnet : subnet.id if subnet.availability_zone != "${var.regionDefault}e"]
   disk_size       = 50
@@ -16,14 +16,14 @@ resource "aws_eks_node_group" "eks-node-backoffice" {
   }
 
   remote_access {
-    ec2_ssh_key = var.accessConfig
+    ec2_ssh_key               = var.accessConfig
     source_security_group_ids = [aws_security_group.backend_sg_backoffice.id]
   }
 }
 
 resource "aws_eks_node_group" "eks-node-pagamento" {
   cluster_name    = aws_eks_cluster.eks-cluster-pagamento.name
-  node_group_name = "${var.nodeGroup}-node-group--pagamento"
+  node_group_name = "${var.nodeGroup}-node-group-pagamento"
   node_role_arn   = var.labRole
   subnet_ids      = [for subnet in data.aws_subnet.subnet : subnet.id if subnet.availability_zone != "${var.regionDefault}e"]
   disk_size       = 50
@@ -38,14 +38,14 @@ resource "aws_eks_node_group" "eks-node-pagamento" {
   }
 
   remote_access {
-    ec2_ssh_key = var.accessConfig
+    ec2_ssh_key               = var.accessConfig
     source_security_group_ids = [aws_security_group.backend_sg_pagamento.id]
   }
 }
 
 resource "aws_eks_node_group" "eks-node-pedido" {
   cluster_name    = aws_eks_cluster.eks-cluster-pedido.name
-  node_group_name = "${var.nodeGroup}-node-group--pedido"
+  node_group_name = "${var.nodeGroup}-node-group-pedido"
   node_role_arn   = var.labRole
   subnet_ids      = [for subnet in data.aws_subnet.subnet : subnet.id if subnet.availability_zone != "${var.regionDefault}e"]
   disk_size       = 50
@@ -61,7 +61,7 @@ resource "aws_eks_node_group" "eks-node-pedido" {
   }
 
   remote_access {
-    ec2_ssh_key = var.accessConfig
+    ec2_ssh_key               = var.accessConfig
     source_security_group_ids = [aws_security_group.backend_sg_pedido.id]
   }
 }
@@ -83,7 +83,7 @@ resource "aws_eks_node_group" "eks-node-producao" {
   }
 
   remote_access {
-    ec2_ssh_key = var.accessConfig
+    ec2_ssh_key               = var.accessConfig
     source_security_group_ids = [aws_security_group.backend_sg_producao.id]
   }
 }
--- sg_sql_server.tf
+++ sg_sql_server.tf
@@ -1,23 +1,23 @@
 resource "aws_security_group" "backend_sg_sql" {
-    name        = "SG-${var.projectName}-SQL-SERVER"
-    description = "Security group for SQL Server instance"
-    vpc_id      = ""
+  name        = "SG-${var.projectName}-SQL-SERVER"
+  description = "Security group for SQL Server instance"
+  vpc_id      = ""
 
-    ingress {
-        from_port   = 1433
-        to_port     = 1433
-        protocol    = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
+  ingress {
+    from_port   = 1433
+    to_port     = 1433
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-    egress {
-        from_port   = 0
-        to_port     = 0
-        protocol    = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-    tags = {
-        Name = "SG-${var.projectName}-SQL-SERVER"
-    }
+  tags = {
+    Name = "SG-${var.projectName}-SQL-SERVER"
+  }
 }
